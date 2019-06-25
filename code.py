adc7210c4b01ef0d8b1c86d1edb940e1fe6f1dc2
AppleBoy5 <43555344+AppleBoy5@users.noreply.github.com>
2019-06-25 20:01:36 +1000

    Update code.py

--- code.py
+++ code.py
@@ -1,62 +1,45 @@
 ```
 print('Hello Welcome To The Carnival! \nWe need to gather some information about you before we can give you your ticket')
-#the top line just prints prints a welcoming message to those who are using the program
+
 user_name = input('What Is your name? ')
-#the above line is prompting the user for a name
 
-#the above line is prompting the user for their age
+ages = {1:"1-7",2:"8-14",3:"15-64",4:"65-99",5:"99-120"}
+q = ages
 
-#the below statements give a value to the age which was entered
 def user_age():
   age = int(input('Please Enter Your age '))
-  a = age
+  x = age
+  if x == q[1] or x == q[5]:
+     print("free")
+  if x == q[2] or x == q[4]:
+      print("$6")
+  if x == q[3]:
+          print("$10")
+  if x < q[1] or x > q[5]:
+        print('Pick A Age between 1 and 120')
+        return
 
-  if a < 1:
-    print('Please Enter A Valid Age')
-  elif user_age:
-
-    if a == 1:
-     print('Free')
-    elif a <= 7:
-       print('free')
-
-  if  a > 7:
-    print('$6')
-  elif a <= 14:
-    print('$6')
-
-  if  a > 14:
-    print('$10')
-  elif a <= 64:
-      print('$10')
-
-  if  a > 64:
-    print('$6')
-  #else a <= 99:
-     # print('$6')
-
-  if  a > 99:
-    print('free')
-  elif a >= 120:
-    print('free')
-
-  if  a > 120 :
-    print('boi no way try again')
-  elif user_age():
-      print()
 
 user_age()
 #the below line is prompting the user to pick a ticket between 1 & 100
 def user_ticket_number():
-    ticket_num= int(input('Please pick a number for your ticket between 1-100 :-) '))
-    x = ticket_num
-    if x < 1 or x > 100:
-        print('Please Pick a number between 1 & 100!')
-    else:
-        print('You Picked ticket number ' + str(user_ticket_number))
-user_ticket_number()
+  min_num = 1
+  mi = min_num
+  max_num = 100
+  ma = max_num
+
+  ticket = int(input("Please Pick A Ticket Number between 1 and 100 "))
+  t = int(ticket)
+  while t < mi or t > ma:
+   print("Please Choose Another Number")
+  if t > mi or t < ma:
+    print(t)
+    return
+print(user_ticket_number())
+
 def print_it():
-    print('Hello ' + user_name + ' it looks like your ticket price is ' + str(user_age) + '')
+    print('Hello ' + user_name + ' it looks like your ticket price is ' + str(user_age()) + '')
 
 print(print_it())
+
 ```
